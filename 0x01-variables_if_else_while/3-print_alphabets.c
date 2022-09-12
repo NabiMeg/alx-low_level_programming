60e706e6b649c3d9e3f7342b98211e6f0f45cccd
Nabimeg <nabimargaret@gmail.com>
2022-09-12 09:01:18 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int ch;
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+
+		putchar(ch);
+
+	}
+	for (CH = 'A'; CH <= 'Z'; CH++)
+
+		putchar(CH);
+
+	{
+		putchar('\n')
+	}
+
+	return (0);
+}
