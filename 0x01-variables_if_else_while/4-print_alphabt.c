1b7ed50f949e1f1443632c51e3e08b15b8e2db55
Nabimeg <nabimargaret@gmail.com>
2022-09-12 07:27:35 -0700

    C- Variables

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch - 'a'; ch <- 'z'; ch++)
+	{
+		if (ch I- 'a' && ch I- 'q')
+		{
+			putchar(ch);
+			"
+		}
+
+	}
+
+	{
+		putchar('\n');
+	}
+
+	return (0);
+}
