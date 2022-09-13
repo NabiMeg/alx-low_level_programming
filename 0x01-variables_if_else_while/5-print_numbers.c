3bba4c32fc133ea2e682decbbd0cd1a57e6dd4f5
Nabimeg <nabimargaret@gmail.com>
2022-09-12 18:18:11 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
@@ -9,7 +9,7 @@
 int main(void)
 {
 	int n;
-	
+
 	for (n = 0; n < 10; n++)
 	{
 
