b31e0f9de329be8f1a50e191c33f8c6cfce12537
Nabimeg <nabimargaret@gmail.com>
2022-09-14 19:43:27 -0700

    Functions, nested loop

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,9 +8,9 @@ void times_table(void)
 	int num, mult, prod;
 
 	for (num = 0; num <= 9; num++)
-	{
+	
 		_putchar('0');
-		
+
 		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
@@ -21,10 +21,8 @@ void times_table(void)
 			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((prod / 10) + '0')
-
-					_putchar((prod % 10) + '0');
+				_putchar((prod / 10) + '0');
 		}
-		_putchar('\n');
+			_putchar((prod % 10) + '0');
+		}
 	}
-}
